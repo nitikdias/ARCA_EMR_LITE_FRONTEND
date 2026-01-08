19b99e3c5e634f1690a80bfb6e44f49c9e2d007a
Nitik <nitik@arcaai.com>
2026-01-08 15:08:56 +0530

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, X-API-KEY' },
         ],
       },
     ];
@@ -30,7 +30,7 @@ const nextConfig = {
       },
       {
         source: '/api/whisper/:path*',
-        destination: 'http://arca-spark-whisper-stt.eastus.azurecontainer.io:5005/:path*',
+        destination: 'https://whisper-stt-fjbfhxeyhwerhuab.z03.azurefd.net/:path*',
       },
     ];
   },
